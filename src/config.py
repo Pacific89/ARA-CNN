29425da0e3889d6652200b181ca21cc8f4f41628
Pacific89 <philipp.faulhammer@medunigraz.at>
2021-07-12 12:03:58 +0200

    4 classes only

--- src/config.py
+++ src/config.py
@@ -7,14 +7,10 @@ CHANNELS = 1 if COLOR_TYPE == 'grayscale' else 3
 
 # Main class dictionary - this needs to match folder names in your training dataset
 CLASS_DICT = {
-    "01_TUMOR": 0,
     "02_STROMA": 1,
-    "03_COMPLEX": 2,
     "04_LYMPHO": 3,
-    "05_DEBRIS": 4,
     "06_MUCOSA": 5,
     "07_ADIPOSE": 6,
-    "08_EMPTY": 7,
 }
 
 # Default optimizer
